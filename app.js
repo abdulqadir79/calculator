81c5cd1c756d9cbf55b793ebe3422a9f00f326b8
Abdul Qadir <87005175+abdulqadir79@users.noreply.github.com>
2024-03-01 16:44:57 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,58 @@
+//Calculator
+
+// // Arithemetic Operator
+
+
+//document.write(8+4)
+//var num1=8;
+//var num2=4;
+//var sum=num1+num2;
+//console.log(sum)
+
+//document.write(8-4)
+//var num1=8;
+//var num2=4;
+//var sub=num1-num2;
+//console.log(sub)
+
+//document.write(8*4)
+//var num1=8;
+//var num2=4;
+//var mul=num1*num2;
+//console.log(mul)
+
+//document.write(8/4)
+//var num1=8;
+//var num2=4;
+//var div=num1-num2;
+//console.log(div)
+
+//document.write(8%4)
+//var num1=8;
+//var num2=4;
+//var reminder=num1%num2;
+//console.log(reminder)
+
+
+
+
+// // prompt Operator
+
+
+//let num1 = + prompt ("Enter Number1");
+//let num2 = + prompt ("Enter Number2");
+ //let sum = num1 + num2;
+ //document.write("Sum Of",sum,"Number","<br/>");
+
+// let num1 = - prompt ("Enter Number1");
+ //let num2 = - prompt ("Enter Number2");
+ //let sub = num1 - num2;
+ //document.write("Sub of", sub,"Number","<br/>");
+ 
+
+ //let div = num1 / num2;
+ //document.write("Divide of Number", div,"<br/>");
+
+
+ //let reminder = num1 / num2;
+ //document.write("Reminder of Number", Reminder,"<br/>");
